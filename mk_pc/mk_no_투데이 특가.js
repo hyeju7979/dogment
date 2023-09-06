a2cf227bb83e4126d1e72456821ff0da19673ed3
GwanHooPark <hooho.dev@gmail.com>
2023-09-07 00:45:37 +0900

    메이크샵 > pc > Full Page 삭제 버전

---
+++ mk_pc/mk_no_투데이 특가.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+    //베스트
+    jQuery('.prd_week_best .item-cont').bxSlider({                                        
+        auto: true,
+        pager: true,
+        controls: true,
+        speed : 1000,                                        
+        pause : 5000,
+        minSlides : 3,
+        maxSlides : 3,
+        slideWidth : 317
+        ,onSliderLoad: function(){
+			$(".prd_week_best .item-cont").css("visibility", "visible").animate({opacity:1});
+		}
+    });
+});
